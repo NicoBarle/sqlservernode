227121a771e52084afa61a64ef79ca6ae2c62922
NicoBarle <barletta.nicolas@iisgalvanimi.edu.it>
2020-02-06 13:38:47 +0000

    06.3

--- RestFull-CRUD/routes/users.js
+++ RestFull-CRUD/routes/users.js
@@ -8,32 +8,50 @@ const config = {
   database: 'barletta.nicolas', //(Nome del DB)
 }
 
-router.get('/attori', function(req, res, next) {
-  sql.connect(config, err => {
-    if(err) console.log(err);  // ... error check
-    
-    // Query
-    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query('select * from [dbo].[cr-unit-attributes]', (err, result) => {
-        if (err) console.log(err); // ... error checks
-        res.send(result);  //Invio il risultato
+let executeQuery = function (res, query, next) {
+  sql.connect(config, function (err) {
+    if (err) { //Display error page
+      console.log("Error while connecting database :- " + err);
+      res.status(500).json({success: false, message:'Error while connecting database', error:err});
+      return;
+    }
+    var request = new sql.Request(); // create Request object
+    request.query(query, function (err, result) { //Display error page
+      if (err) {
+        console.log("Error while querying database :- " + err);
+        res.status(500).json({success: false, message:'Error while querying database', error:err});
+        sql.close();
+        return;
+      }
+      res.send(result.recordset); //Il vettore con i dati è nel campo recordset (puoi loggare result per verificare)
+      sql.close();
     });
+
   });
-});
+}
 
 /* GET users listing. */
-router.get('/attori/:name', function(req, res, next) {
-  sql.connect(config, err => {
-    if(err) console.log(err);  // ... error check
-    
-    // Query
-    let sqlRequest = new sql.Request();  //Oggetto che serve a creare le query
-    sqlRequest.query(`select * from [dbo].[cr-unit-attributes] where Unit = '${req.params.name}'`, (err, result) => {
-        if (err) console.log(err); // ... error checks
-        console.log(req.params.name);
-        res.send(result);  //Invio il risultato
-    });
-  });
+router.get('/', function (req, res, next) {
+  let sqlQuery = "select * from dbo.[cr-unit-attributes]";
+  executeQuery(res, sqlQuery, next);
+});
+
+router.get('/search/:name', function (req, res, next) {
+  let sqlQuery = `select * from dbo.[cr-unit-attributes] where Unit = '${req.params.name}'`;
+  executeQuery(res, sqlQuery, next);
+});
+
+router.post('/', function (req, res, next) {
+  // Add a new Unit  
+  let unit = req.body;
+  if (!unit) {  //Qui dovremmo testare tutti i campi della richiesta
+    res.status(500).json({success: false, message:'Error while connecting database', error:err});
+    return;
+  }
+  let sqlInsert = `INSERT INTO dbo.[cr-unit-attributes] (Unit,Cost,Hit_Speed) 
+                     VALUES ('${unit.Unit}','${unit.Cost}','${unit.Hit_Speed}')`;
+  executeQuery(res, sqlInsert, next);
+  res.send({success:true, message: "unità inserita con successo", unit: unit})
 });
 
 module.exports = router;
